1ab35ec8cfa1e42093b6de7d157f9d585f35b6bf
Syed Najmuddin <aghasyednajmuddin@gmail.com>
2026-06-15 09:15:12 -0700

    Class 2 and 3 codes added

---
+++ class3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    //variables
+    int red;
+    float redb;
+    char redc;
+    string redd;
+    int sum;
+
+    /*red = 45;
+    redb = 402.56;
+    redc = 'y';
+    redd = "apple";
+    cout<<red<< endl;
+    cout<<redb<< endl;
+    cout<<redc<< endl;
+    cout<<redd<< endl;*/
+
+
+   cin>>red;
+   cin>> redb;
+   
+   cout<<red+redb;
+
+    return 0;
+
+
+
+
+
+
+
+
+}
