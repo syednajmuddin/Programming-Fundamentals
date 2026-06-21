343762d1ba5145287e0007ac212b324b61bba429
Syed Najmuddin <aghasyednajmuddin@gmail.com>
2026-06-21 07:16:55 -0700

    Add class5.cpp file

---
+++ class5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int marks;
+    cout<<"enter the marks\n ";
+    cin>>marks;
+    if (marks >= 90) cout<< "Grade A+";
+    if (marks >= 80) cout<<"Grade A";
+    if (marks >=65) cout<<"Grade B";
+    if (marks >=50) cout<<"Grade C";
+    if (marks <=40) cout<<"fail";
+
+    
+    
+    
+
+
+
+
+
+
+
+    return 0;
+}
